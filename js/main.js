3366d9471ebcb98c80a57ec219cb742804f89adf
Megh Vakharia <Megh@Meghs-MacBook-Pro.local>
2013-12-07 21:26:51 -0800

    Fixed geolocation with ipinfo fallback

--- js/main.js
+++ js/main.js
@@ -23,94 +23,89 @@ $(function() {
 	// tile.hide();
 	tile.appendTo(".tiles");
 	// tile.fadeIn(4000);
+	var geocoder;
 	var city;
 	get_location();
-	// $.get("http://ipinfo.io", function(response) {
- //    	console.log(response.ip, response.city);
- //    	$("#city").empty().hide().html(response.city).fadeIn('slow');
-	// }, "jsonp");
-	// $.each(mood, function(index, value) {
-	// 	tile = $(".templates .list-item").clone();
-	// 	alert(this);
-	// 	var content = tile.find(".tile-4");
-	// 	content.html(this);
-	// 	$(".tiles").hide().append(tile).fadeIn();
-	// });  
-
-	//time to handle the map service
-	if (navigator && navigator.geolocation) {
-        updateMessage('I am now obtaining your current position...');
-
-        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, 
-            {enableHighAccuracy: true});
-    }
-    else {
-        updateMessage("Sorry, your shit is BROKEN SUCKA!");
-    }
 }); //onload
 
 function get_location() {
 	console.log("Getting location");
 	if (Modernizr.geolocation) {
-		navigator.geolocation.getCurrentPosition(alertCoords);
+		navigator.geolocation.getCurrentPosition(successFunction);
 	} else {
 		// no native support; maybe try a fallback?
 	}
 }
 
-function alertCoords(position) {
-	var latitude = position.coords.latitude;
-  	var longitude = position.coords.longitude;
-  	alert(latitude + ", " + longitude);
-  	geocoder = new google.maps.Geocoder();
-  	var latlng = new google.maps.LatLng(latitude, longitude);
-  	console.log(latlng);
-  	geocoder.geocode({location: latlng},
-  		function(results, status) {
-  			if(status === google.maps.GeocoderStatus.OK) {
-  				if(results[1]) {
-  					alert(results[0].formatted_address);
-  					for(int i = 0; i < results[0].address_components.length; i++) {
-  						for(int j = 0; j < results[0].address_components[i].types.length; j++) {
-  							if(results[0].address_components[i].types[j] === "administrative_area_level_3") {
-  								city = results[0].address_components[i].long_name;
-  								break;
-  							}
-  						}
-  					}
-  				}
-  				alert("City is:" + city);
-  			}
-  		});
+function successFunction(position) {
+	var lat = position.coords.latitude;
+	var lng = position.coords.longitude;
+	alert(lat + ", " + lng);
+	codeLatLng(lat, lng);
 }
 
+function codeLatLng(lat, lng) {
+	var latlng = new google.maps.LatLng(lat, lng);
+	geocoder = new google.maps.Geocoder();
+	geocoder.geocode({
+		'latLng': latlng
+	}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			console.log(results)
+			if (results[1]) {
+				//formatted address
+				alert(results[0].formatted_address)
+				//find country name
+				for (var i = 0; i < results[0].address_components.length; i++) {
+					for (var b = 0; b < results[0].address_components[i].types.length; b++) {
+						//there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+						if (results[0].address_components[i].types[b] == "locality") {
+							//this is the object you are looking for
+							alert(results[0].address_components[i].long_name);
+							city = results[0].address_components[i].long_name;
+							$("#city").hide().html(city).fadeIn();
+							break;
+						}
+					}
+				}
+			} else {
+				$.get("http://ipinfo.io", function(response) {
+					console.log(response.city, response.country);
+					$("#city").hide().html(city).fadeIn();
+				}, "jsonp");
+			}
+		} else {
+			alert("Geocoder failed due to: " + status);
+		}
+	});
+}
 
 function onGeoSuccess(position) {
-    var ll = new google.maps.LatLng(position.coords.latitude,
-        position.coords.longitude);
+	var ll = new google.maps.LatLng(position.coords.latitude,
+		position.coords.longitude);
 
-    var mapOptions = {
-        center: ll,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+	var mapOptions = {
+		center: ll,
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
 
-    var map = new google.maps.Map($('.map-container')[0], mapOptions);
+	var map = new google.maps.Map($('.map-container')[0], mapOptions);
 
-    var marker = new google.maps.Marker({
-        map: map,
-        position: ll,
-        title: 'You are here!'
-    });
+	var marker = new google.maps.Marker({
+		map: map,
+		position: ll,
+		title: 'You are here!'
+	});
 
-     updateMessage("You should go eat at");
+	updateMessage("You should go eat at");
 
 }
 
 function onGeoError(err) {
-    updateMessage("Sorry, your shit is BROKEN SUCKA!");
+	updateMessage("Sorry, your shit is BROKEN SUCKA!");
 }
 
 function updateMessage(msg) {
-    $('.map-message').html(msg);
+	$('.map-message').html(msg);
 }
