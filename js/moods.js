4449b28a08427c1cb800c997897d67be7a1bcdf5
Megh Vakharia <Megh@Meghs-MacBook-Pro.local>
2013-12-09 19:54:38 -0800

    Changed stroke color

--- js/moods.js
+++ js/moods.js
@@ -3,25 +3,25 @@
 		"mood": "Angry",
 		"type": "spicy",
 		"image": "img/angry.png",
-		"map_img": "img/dest_angry.png",
+		"map_img": "img/dest_angry_fill.png",
 		"categories": "cajun,tex-mex,food_court,foodstands,hotdogs,fishnchips"
 	}, {
 		"mood": "Happy",
 		"type": "healthy",
 		"image": "img/happy.png",
-		"map_img": "img/dest_happy.png",
+		"map_img": "img/dest_happy_fill.png",
 		"categories": "gluten_free,raw_food,salad,seafood,vegan,vegetarian"
 	}, {
 		"mood": "Sad",
 		"type": "healthy",
 		"image": "img/sad.png",
-		"map_img": "img/dest_sad.png",
+		"map_img": "img/dest_sad_fill.png",
 		"categories": "newamerican,tradamerican,bbq,breakfast_brunch,buffets,burgers,cafeteria,cheesesteaks,chicken_wings,comfortfood,delis,hotpot,sandwiches,soulfood,soup,southern"
 	}, {
 		"mood": "Baked",
 		"type": "awesome",
 		"image": "img/baked.png",
-		"map_img": "img/dest_baked.png",
+		"map_img": "img/dest_baked_fill.png",
 		"categories": "cafes,creperies,burgers,comfortfood,hotdog,pizza"
 	}]
 }
